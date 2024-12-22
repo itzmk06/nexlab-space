acdbea4d1efa0e778b2487185cbe61cf501bc8d9
Manoj Kumar M <itzmk06@gmail.com>
2024-12-22 19:20:22 +0530

    nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
-import { hostname } from "os";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverActions: true,
     mdxRs: true,
-    serverComponentsExternalPackages: ["mongoose"],
-    serverActions: true, 
+    serverComponentsExternalPackages: ['mongoose']
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "*",
-      },
-      { protocol: "https", hostname: "*" },
-    ],
-  },
-};
+        protocol: 'https',
+        hostname: '*'
+      }, 
+      {
+        protocol: 'http',
+        hostname: '*'
+      }, 
+    ]
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
